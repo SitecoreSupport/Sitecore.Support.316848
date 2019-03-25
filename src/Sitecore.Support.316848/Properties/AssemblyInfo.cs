7d33ac344f4785d22ee2575dedff871233f0858b
DK\vwo <vwo@sitecore.net>
2019-03-25 15:09:59 +0800

    Unable to scroll message body content when using 'Existing Page' template

---
+++ src/Sitecore.Support.316848/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.316848")]
+[assembly: AssemblyProduct("Sitecore.Support.316848")]
+[assembly: ComVisible(false)]
